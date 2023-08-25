071b410c15459f0508583b3bd6b8f130aa5f46f1
Gabriel Sena <bi_sena1@hotmail.com>
2023-08-25 19:09:25 -0300

    Adicionar arquivos de projeto.

---
+++ CursoCSharpe/Fundamentos/VariaveisEFundamentos.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CursoCSharpe.Fundamentos
+{
+    internal class VariaveisEFundamentos
+    {
+        public static void Executar()
+        {
+            // Area da circunferencia
+            double raio = 4.5;
+            const double PI = 3.14;
+
+            raio = 5.5;
+
+            double area= PI * raio * raio;
+            Console.WriteLine(area);
+            Console.WriteLine("A soma é " + area);
+
+            // Tipos internos
+
+            bool estaChovendo = true;
+            Console.WriteLine("Está chovendo ? " +  estaChovendo);
+
+            byte idade = 22;
+            Console.WriteLine("Idade " + idade);
+
+            sbyte saldoDeGols = sbyte.MinValue;
+            Console.WriteLine("Saldo de gols " + saldoDeGols);
+
+            short salario = short.MaxValue;
+            Console.WriteLine("Salario " + salario);
+
+            int menorValorInt = int.MinValue; // Mais Usado dos inteiros !
+            Console.WriteLine("Menor int " + menorValorInt);
+
+            uint populacaoBrasileira = 207_600_000;
+            Console.WriteLine("População Brasileira " + populacaoBrasileira);
+
+            long menorValorLong = long.MinValue;
+            Console.WriteLine("Menor long " + menorValorLong);
+
+            ulong populacaoMundial = 7_600_000_000;
+            Console.WriteLine("População Mundial " + populacaoMundial);
+        }
+    }
+}
---
+++ CursoCSharpe/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+using CursoCSharpe.Fundamentos;
+
+namespace CursoCSharpe {
+    class Program {
+        static void Main(string[] args) {
+            var central = new CentralDeExercicios(new Dictionary<string, Action>() {
+                {"Primeiro Programa - Fundamentos", PrimeiroPrograma.Executar},
+                {"Segundo Programa - Comentarios", Comentarios.Executar },
+                {"Terceiro Programa - VariaveisEFundamentos", VariaveisEFundamentos.Executar},
+            });
+
+            central.SelecionarEExecutar();
+        }
+    }
+}
